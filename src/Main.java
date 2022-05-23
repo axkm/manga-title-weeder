dea9343af5361ef49a24f4bb216804a0ef45eded
Alex <akim8@emich.edu>
2022-05-22 23:35:10 -0400

    get list of files in data folder

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+import java.io.File;
+import java.util.Scanner;
 
 public class Main {
+	
+	private final static String dataPath = "./src/manga data/";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		final File folder = new File(dataPath);
+		listFilesForFolder(folder);
+	}
+	
+	public static void listFilesForFolder(final File folder) {
+	    for (final File fileEntry : folder.listFiles()) {
+	        if (fileEntry.isDirectory()) {
+	            listFilesForFolder(fileEntry);
+	        } else {
+	            System.out.println(fileEntry.getName());
+	        }
+	    }
 	}
 
 }
